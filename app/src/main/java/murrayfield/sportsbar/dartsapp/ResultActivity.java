cf3140572b21633d269f18d73f28b6b5a978c7a2
unknown <JOTO@DT001015.local.tmvse.com>
2015-10-21 12:21:36 +0100

    implemented results activity to display real data

--- app/src/main/java/murrayfield/sportsbar/dartsapp/ResultActivity.java
+++ app/src/main/java/murrayfield/sportsbar/dartsapp/ResultActivity.java
@@ -4,13 +4,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RelativeLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
-public class ResultActivity extends BaseActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import murrayfield.sportsbar.dartsapp.request.AsyncResponse;
+import murrayfield.sportsbar.dartsapp.request.GetJSONData;
+
+public class ResultActivity extends BaseActivity implements AsyncResponse {
+
+    GetJSONData asyncTask = new GetJSONData();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_result);
+
+
+        if (!isConnectedToInternet()) {
+            // popup to inform user of no internet connection
+            onCreateDialogNoInternetConnection().show();
+        } else {
+            setContentView(R.layout.activity_result);
+            asyncTask.delegate = this;
+            asyncTask.execute("http://nodejs-dartsapp.rhcloud.com/results", "RESULTS");
+        }
     }
 
     @Override
@@ -59,5 +81,109 @@ public class ResultActivity extends BaseActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void processFinish(String output, Endpoint endpoint) {
+
+        if (endpoint == Endpoint.RESULTS) {
+            TableLayout table = new TableLayout(this);
+
+            JSONObject jObject;
+            JSONArray jArray = null;
+            try {
+                jObject = new JSONObject(output);
+                jArray = jObject.getJSONArray("results");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            if (jArray != null) {
+
+                // converting value from dps to pixels using the display scale factor
+                final float scale = this.getResources().getDisplayMetrics().density;
+                int dateWidthPx = (int) (250 * scale + 0.5f);
+                int groupWidthPx = (int) (100 * scale + 0.5f);
+                int playerWidthPx = (int) (140 * scale + 0.5f);
+                int legsWonWidthPx = (int) (80 * scale + 0.5f);
+
+                for (int i = 0; i < jArray.length(); i++) {
+
+                    JSONObject objectInArray;
+                    String weekDate;
+                    String group;
+                    String playerOne;
+                    String playerOneLegsWon;
+                    String playerTwo;
+                    String playerTwoLegsWon;
+                    try {
+                        objectInArray = jArray.getJSONObject(i);
+
+                        weekDate = objectInArray.getString("weekDate");
+                        group = objectInArray.getString("group");
+
+                        playerOne = objectInArray.getString("playerOne");
+
+                        String[] playerNameArray;
+
+                        // This ensures only forename and surname are displayed in the table
+                        playerNameArray = playerOne.split("\\s+");
+                        if (playerNameArray.length > 2) {
+                            playerOne = playerNameArray[0] + " " + playerNameArray[1];
+                        }
+                        // This ensures only forename and surname are displayed in the table
+                        playerTwo = objectInArray.getString("playerTwo");
+                        playerNameArray = playerTwo.split("\\s+");
+                        if (playerNameArray.length > 2) {
+                            playerTwo = playerNameArray[0] + " " + playerNameArray[1];
+                        }
+
+                        playerOneLegsWon = Integer.toString(objectInArray.getInt("playerOneLegsWon"));
+                        playerTwoLegsWon = Integer.toString(objectInArray.getInt("playerTwoLegsWon"));
+
+                        TableRow fixtureRow = new TableRow(this);
+
+                        TextView weekDateLabel = new TextView(this);
+                        weekDateLabel.setText(weekDate);
+                        weekDateLabel.setWidth(dateWidthPx);
+
+                        TextView groupLabel = new TextView(this);
+                        groupLabel.setText(group);
+                        groupLabel.setWidth(groupWidthPx);
+
+                        TextView playerOneLabel = new TextView(this);
+                        playerOneLabel.setText(playerOne);
+                        playerOneLabel.setWidth(playerWidthPx);
+
+                        TextView playerOneLegsWonLabel = new TextView(this);
+                        playerOneLegsWonLabel.setText(playerOneLegsWon);
+                        playerOneLegsWonLabel.setWidth(legsWonWidthPx);
+
+                        TextView playerTwoLabel = new TextView(this);
+                        playerTwoLabel.setText(playerTwo);
+                        playerTwoLabel.setWidth(playerWidthPx);
+
+                        TextView playerTwoLegsWonLabel = new TextView(this);
+                        playerTwoLegsWonLabel.setText(playerTwoLegsWon);
+                        playerTwoLegsWonLabel.setWidth(legsWonWidthPx);
+
+                        fixtureRow.addView(weekDateLabel);
+                        fixtureRow.addView(groupLabel);
+                        fixtureRow.addView(playerOneLabel);
+                        fixtureRow.addView(playerOneLegsWonLabel);
+                        fixtureRow.addView(playerTwoLabel);
+                        fixtureRow.addView(playerTwoLegsWonLabel);
+
+                        table.addView(fixtureRow);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            RelativeLayout main = (RelativeLayout)findViewById(R.id.result_activity);
+            main.addView(table);
+        }
     }
 }
